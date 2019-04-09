3fc7699ee495c1d767b278d752ceb3ed93fc3932
emmanuelJulia <emmanuel.julia.external@sigfox.com>
2019-04-09 16:59:30 +0200

    - Deleted Home components

--- src/main/java/com/ejulia/bookworm/model/Home.java
+++
@@ -1,9 +0,0 @@
-package com.ejulia.bookworm.model;
-
-public class Home {
-    private final String welcomeTemplate = "Welcome to BookWorm %s!";
-
-    public String getWelcomeMessage(String userName) {
-        return String.format(welcomeTemplate, userName);
-    }
-}
